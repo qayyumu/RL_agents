717bebe15a10c22b46e311bc2ee919a16e81948d
usmanqayyum <mrusmanqayyum@gmail.com>
2018-11-22 11:01:15 +0000

    policy gradient simple

---
+++ policy_gradient_gym.py
@@ -0,0 +1,23 @@
+import tensorflow as tf
+import tensorflow.contrib.slim as slim
+import numpy as np
+import gym
+import matplotlib.pyplot as plt
+%matplotlib inline
+
+try:
+    xrange = xrange
+except:
+    xrange = range
+
+env = gym.make('CartPole-v0')
+
+
+
+
+
+
+
+
+
+
